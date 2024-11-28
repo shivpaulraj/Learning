562cf93913dc33764b86078f93f64394840180c6
Sivasubramanian Paulraj <shivpaulraj@gmail.com>
2024-11-29 00:55:51 +0530

    Fix form Errors

--- angular-latest-development/src/app/form/form.component.ts
+++ angular-latest-development/src/app/form/form.component.ts
@@ -1,28 +1,72 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+// @Component({
+//   selector: 'app-first',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './form.component.html',
+//   styleUrl: './form.component.scss'
+// })
+// export class FormComponent {
+// password: any;
+// SubmitForm() {
+//  if(this.profileForm.valid){
+//   console.log(this.profileForm);
+//  }
+// }
+// Define a function with two regular parameters and one rest parameter:
+//login 
+//home
+// profileForm:FormGroup;
+// constructor(){
+//   this.profileForm=new FormGroup({
+//     userID:new FormControl(''), 
+//     password:new FormControl('')
+//   })
+// }
+
+// }
+
+
 @Component({
-  selector: 'app-first',
-  standalone: true,
-  imports: [],
+  selector: 'app-login',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrls: ['./form.component.scss'],
+  standalone: true,
+  imports: [
+    NgIf
+],
 })
 export class FormComponent {
-password: any;
+  password: any;
 SubmitForm() {
  if(this.profileForm.valid){
   console.log(this.profileForm);
  }
 }
-// Define a function with two regular parameters and one rest parameter:
-//login 
-//home
-profileForm:FormGroup;
-constructor(){
-  this.profileForm=new FormGroup({
-    userID:new FormControl(''), 
-    password:new FormControl('')
-  })
-}
 
+
+  profileForm:FormGroup;
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.profileForm=new FormGroup({
+      userID:new FormControl(''), 
+      password:new FormControl('')
+    })
+
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted:', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- angular-latest-development/src/app/form/form.module.ts
+++ angular-latest-development/src/app/form/form.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule, FormsModule
-  ]
+  imports: [CommonModule,
+    BrowserModule,ReactiveFormsModule,
+    FormsModule
+  ],
+  exports: [ FormsModule,
+    ReactiveFormsModule]
 })
-export class FirstModule { }
+
+export class AppModule {}
